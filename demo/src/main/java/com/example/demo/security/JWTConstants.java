2c2534b4e63fb3bfe909aa616d672adcf5fbf215
Thanhtan101103 <161003191+baptt123@users.noreply.github.com>
2024-08-23 20:23:47 +0700

    cấu trúc code:cấu hình spring boot security json web token(23/8/2024)

---
+++ demo/src/main/java/com/example/demo/security/JWTConstants.java
@@ -0,0 +1,7 @@
+package com.example.demo.security;
+
+public class JWTConstants {
+    //secret key cho jwt
+    public final static String JWT_SECRET = "loading_login"; // Thay thế bằng secret key của bạn
+    public final static long JWT_EXPIRATION = 604800000L;//token có hiệu lực 7 ngày
+}
---
+++ demo/src/main/java/com/example/demo/userdetail/CustomUserDetail.java
@@ -0,0 +1,62 @@
+package com.example.demo.userdetail;
+import com.example.demo.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class CustomUserDetail implements UserDetails {
+    private User user;
+
+    public CustomUserDetail(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+
+}
