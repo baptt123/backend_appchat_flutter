66b0e8fd1e4a3fab33f5d67e0cdb1267a28f2595
Thanhtan101103 <161003191+baptt123@users.noreply.github.com>
2024-09-02 10:08:28 +0700

    cấu hình:cấu hình để deploy trong môi trường docker(2/9/2024)

--- demo/src/main/java/com/example/demo/repository/CommentRepository.java
+++ demo/src/main/java/com/example/demo/repository/CommentRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Comment;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends CrudRepository<Comment,Integer> {
 
 }
