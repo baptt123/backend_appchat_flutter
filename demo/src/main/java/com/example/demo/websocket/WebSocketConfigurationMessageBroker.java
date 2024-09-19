11775777fcc230087d3570a4effa2244dcc78418
Thanhtan101103 <161003191+baptt123@users.noreply.github.com>
2024-09-19 09:48:37 +0700

    cấu trúc code:cấu hình websocket dạng messagebroker(19/9/2024)

--- demo/src/main/java/com/example/demo/websocket/WebSocketConfigurationMessageBroker.java
+++ demo/src/main/java/com/example/demo/websocket/WebSocketConfigurationMessageBroker.java
@@ -4,18 +4,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 
-@Configuration
+//@Configuration
 //@EnableWebSocketMessageBroker
 public class WebSocketConfigurationMessageBroker implements WebSocketMessageBrokerConfigurer {
 //    @Override
 //    public void registerStompEndpoints(StompEndpointRegistry registry) {
-//        registry.addEndpoint("/ws");
+//        registry.addEndpoint("/ws").setAllowedOrigins("*");
 //    }
 //
 //    @Override
 //    public void configureMessageBroker(MessageBrokerRegistry registry) {
 //        registry.enableSimpleBroker("/topic");//tù server gửi về lại client
 //        registry.setApplicationDestinationPrefixes("/app");//từ client gửi xuống server
+//        registry.setUserDestinationPrefix("/topic");
+//        registry.configureBrokerChannel();
+//        registry.enableSimpleBroker("/app");
 //    }
 
 }
